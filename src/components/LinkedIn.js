e2d25a641bdf2f8070337731bc7d808827b7484c
Penelope Jungreis <penelope.jungreis@gmail.com>
2021-02-23 15:33:22 +0000

    made linkedin cover photo

---
+++ src/components/LinkedIn.js
@@ -0,0 +1,185 @@
+import React from 'react'
+//import { Link } from 'react-router-dom'
+
+function Skills() {
+
+  return (
+    <>
+      <main className="skills-page">
+        <h1>Special Talents</h1>
+        <ul>
+          <li>
+            <i className="devicon-react-original"></i>
+            <p>React</p>
+          </li>
+          <li>
+            <i className="devicon-javascript-plain"></i>
+            <p>JavaScript</p>
+          </li>
+          <li>
+            <i className="devicon-python-plain"></i>
+            <p>Python</p>
+          </li>
+          <li>
+            <i className="devicon-css3-plain"></i> 
+            <p>CSS3</p>
+          </li>
+          <li>
+            <i className="devicon-html5-plain"></i>
+            <p>HTML5</p>
+          </li>
+          <li>
+            <i className="devicon-nodejs-plain"></i>
+            <p>Node.js</p>
+          </li>
+          <li>
+            <i className="devicon-express-original"></i>
+            <p>Express.js</p>
+          </li>
+          <li>
+            <i className="devicon-mongodb-plain"></i>
+            <p>MongoDB</p>
+          </li>
+          <li>
+            <i className="devicon-django-plain"></i>
+            <p>Django</p>
+          </li>
+          <li>
+            <i className="devicon-postgresql-plain"></i>
+            <p>PostgresSQL</p>
+          </li>
+          <li>
+            <i className="devicon-sass-original"></i>
+            <p>SASS</p>
+          </li>
+          <li>
+            <i className="devicon-github-original"></i>
+            <p>GitHub</p>
+          </li>
+          <li>
+            <i className="material-icons">view_headline</i>
+            <p>StackOverflow</p>
+          </li>
+          <li>
+            <i className="material-icons">groups</i>
+            <p>MDN</p>
+          </li>
+          <li>
+            <i className="material-icons">laptop_mac</i>
+            <p>Netlify</p>
+          </li>
+          <li>
+            <i className="devicon-heroku-original"></i>
+            <p>Heroku</p>
+          </li>
+          <li>
+            <i className="material-icons">code</i>
+            <p>VS Code</p>
+          </li>
+          <li>
+            <i className="devicon-git-plain"></i>
+            <p>Git</p>
+          </li>
+          <li>
+            <i className="devicon-slack-plain"></i>
+            <p>Slack</p>
+          </li>
+          <li>
+            <i className="material-icons">flutter_dash</i>
+            <p>Hootsuite</p>
+          </li>
+          <li>
+            <i className="material-icons">album</i>
+            <p>Insomnia</p>
+          </li>
+          <li>
+            <i className="material-icons">grid_on</i>
+            <p>TablePlus</p>
+          </li>
+          <li>
+            <i className="material-icons">place</i>
+            <p>MapBox</p>
+          </li>
+          <li>
+            <i className="material-icons">wb_cloudy</i>
+            <p>Cloudinary</p>
+          </li>
+          <li>
+            <i className="devicon-firebase-plain"></i>
+            <p>Firebase</p>
+          </li>
+          <li>
+            <i className="devicon-gimp-plain"></i>
+            <p>GIMP</p>
+          </li>
+          <li>
+            <i className="devicon-wordpress-plain"></i>
+            <p>WordPress</p>
+          </li>
+          <li>
+            <i className="devicon-google-plain"></i>
+            <p>Analytics</p>
+          </li>
+          <li>
+            <i className="material-icons">devices_other</i>
+            <p>Responsive Design</p>
+          </li>
+          <li>
+            <i className="material-icons">psychology</i>
+            <p>UX</p>
+          </li>
+          <li>
+            <i className="material-icons">fact_check</i>
+            <p>Project Mgmt</p>
+          </li>
+          <li>
+            <i className="material-icons">cake</i>
+            <p>Event Planning</p>
+          </li>
+          <li>
+            <i className="material-icons">keyboard_voice</i>
+            <p>Public Speaking</p>
+          </li>
+          <li>
+            <i className="material-icons">emoji_people</i>
+            <p>Giving Tours</p>
+          </li>
+          <li>
+            <i className="material-icons">keyboard</i>
+            <p>Writing</p>
+          </li>
+          <li>
+            <i className="material-icons">watch</i> 
+            <p>Scuba Diving</p>
+          </li>
+          <li>
+            <i className="material-icons">downhill_skiing</i>
+            <p>Skiing</p>
+          </li>
+          <li>
+            <i className="material-icons">escalator_warning</i>
+            <p>Social Dancing</p>
+          </li>
+          <li>
+            <i className="material-icons">format_quote</i>
+            <p>Blogging</p>
+          </li>
+          <li>
+            <i className="material-icons">photo_size_select_large</i>
+            <p>Photo Editing</p>
+          </li>
+          <li>
+            <i className="material-icons">dinner_dining</i>
+            <p>Cooking</p>
+          </li>
+          <li>
+            <i className="material-icons">public</i>
+            <p>Traveling</p>
+          </li>
+        </ul>
+      </main>
+    </>
+  )
+}
+
+export default Skills
