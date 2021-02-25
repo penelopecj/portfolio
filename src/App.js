5e7d2d2c700b0e83730031e97352de481376d483
Penelope Jungreis <penelope.jungreis@gmail.com>
2021-02-25 17:17:43 +0000

    added footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './components/Home'
 import Projects from './components/Projects'
 import Skills from './components/Skills'
 import About from './components/About'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route exact path="/skills" component={Skills}/>
         <Route exact path="/about" component={About}/>
       </Switch>
+      <Footer />
     </BrowserRouter>
   )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+
+  return (
+    <footer>
+      <p>Penelope Jungreis &copy; 2021</p>
+    </footer>
+  )
+}
+
+export default Footer
